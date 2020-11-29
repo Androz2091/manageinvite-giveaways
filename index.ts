3db7201e0c90d1bfc38ac063385efca5895498d3
Androz2091 <androz2091@gmail.com>
2020-11-29 10:50:32 +0100

    :bug: Check if the winners are still in the server

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Client, TextChannel, User } from 'discord.js';
+import { Client, Collection, GuildMember, TextChannel, User, Snowflake } from 'discord.js';
 import * as config from './config.json';
 import inquirer from 'inquirer';
 import chalk from 'chalk';
@@ -10,37 +10,44 @@ interface PrizeWinners {
 
 const client = new Client();
 
+let guildFetched = false;
+
 const roll = () => {
     console.clear();
     console.log(`\n`);
-    (client.channels.cache.get(config.messageToRoll.channelID) as TextChannel).messages.fetch(config.messageToRoll.messageID).then((message) => {
-        message.reactions.cache.first()?.fetch().then((reaction) => {
-            reaction.users.fetch().then((users) => {
-                const prizes: PrizeWinners[] = [];
-                config.prizes.forEach((prize) => {
-                    const winners = users.random(prize.count);
-                    prizes.push({
-                        name: prize.name,
-                        winners
+    const channel = (client.channels.cache.get(config.messageToRoll.channelID) as TextChannel);
+    const promise: Promise<Collection<Snowflake, GuildMember>|void> = !guildFetched ? channel.guild.members.fetch() : new Promise((resolve) => resolve());
+    promise.then(() => {
+        channel.messages.fetch(config.messageToRoll.messageID).then((message) => {
+            message.reactions.cache.first()?.fetch().then((reaction) => {
+                reaction.users.fetch().then((users) => {
+                    users = users.filter((user) => reaction.message.guild?.members.cache.has(user.id) || false)
+                    const prizes: PrizeWinners[] = [];
+                    config.prizes.forEach((prize) => {
+                        const winners = users.random(prize.count);
+                        prizes.push({
+                            name: prize.name,
+                            winners
+                        });
+                        console.log(`${chalk.green(prize.name)}`);
+                        console.log(winners.map((winner) => `${winner.tag}`).join(', ')+'\n');
                     });
-                    console.log(`${chalk.green(prize.name)}`);
-                    console.log(winners.map((winner) => `${winner.tag}`).join(', ')+'\n');
+                    inquirer.prompt([
+                        {
+                            name: 'continue',
+                            message: 'Are the winners correct?',
+                            type: 'confirm'
+                        }
+                    ]).then((answers) => {
+                        if (!answers.continue) roll();
+                        else {
+                            const message = prizes.map((prize) => {
+                                return `**${prize.name}**\n\nWon by ${prize.winners.map((winner) => winner.toString()).join(', ')}!`;
+                            }).join('\n\n\n')+'\n\nCongratulations to the winners! :tada:';
+                            (client.channels.cache.get(config.outputChannel) as TextChannel).send(message);
+                        }
+                    })
                 });
-                inquirer.prompt([
-                    {
-                        name: 'continue',
-                        message: 'Are the winners correct?',
-                        type: 'confirm'
-                    }
-                ]).then((answers) => {
-                    if (!answers.continue) roll();
-                    else {
-                        const message = prizes.map((prize) => {
-                            return `**${prize.name}**\n\nWon by ${prize.winners.map((winner) => winner.toString()).join(', ')}!`;
-                        }).join('\n\n\n')+'\n\nCongratulations to the winners! :tada:';
-                        (client.channels.cache.get(config.outputChannel) as TextChannel).send(message);
-                    }
-                })
             });
         });
     });
